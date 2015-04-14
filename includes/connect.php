b1f2b5d7f02348e4e24dfeea65620633b1a9af6f
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-14 12:28:56 -0700

    Finished milestone 6.  Had a few silly errors and re-routed the connection.php file to the "includes" folder.

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');//connecting to to mysqli server
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');//connecting to to mysqli server
 if ($mysqli->connect_error) {//if$mysqli->connect_error then we want it to die and have this message
 die('Connect Error (' . $mysqli->connect_errno. ')'
 	. $mysqli->connect_error);
@@ -8,6 +8,6 @@ die('Connect Error (' . $mysqli->connect_errno. ')'
 else{
 	echo"Connection made";
 }
-$mysqli->(close);
+$mysqli->close();
 
 ?>
