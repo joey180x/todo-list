66b560d815841cbe4dea3728cdb1e0ab9b009ba8
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-16 11:55:46 -0700

    Finished Milestone 8. Added more css for styling, and a few more comments

--- includes/connect.php
+++ includes/connect.php
@@ -2,12 +2,12 @@
 
 $mysqli = new mysqli('localhost', 'root', 'root', 'todo');//connecting to to mysqli server
 if ($mysqli->connect_error) {//if$mysqli->connect_error then we want it to die and have this message
-die('Connect Error (' . $mysqli->connect_errno. ')'
+die('Connect Error (' . $mysqli->connect_errno.')' //die if theres no connecion
 	. $mysqli->connect_error);
 }
 else{
-	echo"Connection made";
+	// echo "Connection made";//echoing connection is made
 }
-$mysqli->close();
+$mysqli->close();//closing mysqli
 
 ?>
