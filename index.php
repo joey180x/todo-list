856f0c0ce795353d73eacf926df70e26edc64f72
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-16 12:06:59 -0700

    Finished Milestone 9. Added stuff to be able to post.

--- index.php
+++ index.php
@@ -16,4 +16,21 @@
 	</form><!--closing form tag-->
 	</div><!--closing divider-->
 </body><!--closing html tag-->
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script>
+	add_task(); //calling the add task function
+
+	function add_task(){
+		$('.add-new-task').submit(function() {
+			var new_task = $('.add-new-task input [name=new-task').val();
+
+			if (new_task != '') {
+				$.post('includes/add-task.php', { task: new_task}, function(data) {
+					$(('add-new-task input[name=new-task]').val();
+						$(data).appendTo('task-list ul').hide().fadeIn();
+				});
+			};
+		})
+	}
+</script>
 </html><!--closing html tag-->
