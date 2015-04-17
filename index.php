7a1aec40428c913d58bc6a20861a3f3aaad7b6de
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-17 12:00:10 -0700

    Fixed error from previous. Working 100% just forgot a period

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 							$task_name = $row["task"];//and task name
 
 							echo '<li>
-							<span>'.$task_name'</span>
+							<span>'.$task_name. '</span>
 							<img id="'.task_id.'" class="delete-button" width="10px" src="images/close.svg"/>
 							</li>';
 						}
