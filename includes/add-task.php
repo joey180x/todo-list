979fd6fe517c6619e6d342aca787992140eedf14
Joseph De La Portilla <josephdlp18@gmail.com>
2015-04-17 19:50:12 -0700

    Tried fixing these errors. Couldn't figure them out. But I have less than before

--- includes/add-task.php
+++ includes/add-task.php
@@ -1,14 +1,14 @@
 <?php
-	$task = strip_tags($_POST['task']);//strip tags off post coming in
+	$task = strip_tags($_POST['tasks']);//strip tags off post coming in
 	$date = date('Y-m-d');//date stamp
 	$time = date('H:i:s');//time including single date
 
 	include('connect.php');
 
-	$mysqli = new mysqli('localhost', 'root', 'root', 'task');//new mysqli connection
-	$mysqli ->query("INSERT INTO tasks VALUES ('', '$task', '$date', '$time')");//insert values into tasks
+	$mysqli = new mysqli('localhost', 'root', 'root', 'todo');//new mysqli connection
+	$mysqli ->query("INSERT INTO todo VALUES ('', '$task', '$date', '$time')");//insert values into tasks
 
-	$query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time'$time' ";//query select from tasks where task and date and time
+	$query = "SELECT * FROM todo WHERE task='$task' and date='$date' and time='$time' ";//query select from tasks where task and date and time
 
 	if ($result = $mysqli->query($query)) {
 		while ($row = $result->fetch_assoc()){
@@ -16,9 +16,6 @@
 			$task_name = $row['task'];
 		}
 }
-
+	echo '<li><span>'.$task_name.'</span><img id"'.$task_id.'" class="delete-button" width="10px" src=images/close.svg" /></li>';
 	$mysqli->close();
-
-	echo '<li><span>'.$task_name.'</span><img id"'.task.id.'" class="delete-button" width="10px" src=images/close.svg" /></li>';
-	
 	?>
